aef76c50abe732e420f97e9f2c5bc06762ed6830
varelacp <124377343+varelacp@users.noreply.github.com>
2023-04-24 11:25:12 +0100

    done

---
+++ models/Movie.model.js
@@ -0,0 +1,19 @@
+const { Schema, model } = require('mongoose');
+
+const movieSchema = new Schema(
+  {
+    title: String,
+    director: String,
+    stars: [String],
+    image: String,
+    description: String,
+    showtimes: [String]
+  },
+  {
+    timestamps: true
+  }
+);
+
+const movieModel = model('Movie', movieSchema);
+
+module.exports = movieModel;
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const Movie = require('../models/Movie.model');
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+router.get('/movies', async (req, res) => {
+  const moviesfromDB = await Movie.find();
+  res.render('movies', { movies: moviesfromDB });
+});
+
+router.get('/movies/:id', async (req, res) => {
+  const movieDetails = await Movie.findById(req.params.id);
+  console.log('movieDetails');
+  res.render('movie-details', { movieDetails });
+});
 
 module.exports = router;
